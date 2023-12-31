8fe4c1dfebbf40011712b86670813e287163e8f4
HP <kumargaurav19999@gmail.com>
2023-12-31 13:55:49 +0530

    new change

--- lib/help/helper.dart
+++ lib/help/helper.dart
@@ -12,7 +12,6 @@ import '../repos/authentication_repository.dart';
 // This class is using for toast messaging and any method call whole app
 class Helper {
   BuildContext? context;
-  DateTime? currentBackPressTime;
 
   Helper.of(BuildContext _context) {
     context = _context;
@@ -58,60 +57,7 @@ class Helper {
                       color: AppColors().colorPrimary(1),
                       fontWeight: FontWeight.w500)),
               onPressed: () {
-
                 context.read<AuthenticationRepository>().logOut();
-
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  ///Quit the app
-  Future<void> showQuitDialog(BuildContext context) async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Exit',
-              style: TextStyle(
-                  fontSize: AppConfig(context).appHeight(2.5),
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500)),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Are you sure you want to Exit?',
-                    style: TextStyle(
-                        fontSize: AppConfig(context).appHeight(2.2),
-                        color: Colors.black87,
-                        fontWeight: FontWeight.w500)),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('No',
-                  style: TextStyle(
-                      fontSize: AppConfig(context).appHeight(2.5),
-                      color: AppColors().colorPrimary(1),
-                      fontWeight: FontWeight.w500)),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Yes',
-                  style: TextStyle(
-                      fontSize: AppConfig(context).appHeight(2.5),
-                      color: AppColors().colorPrimary(1),
-                      fontWeight: FontWeight.w500)),
-              onPressed: () {
-                // navigatorKey.currentState!.pushNamedAndRemoveUntil(('/LoginView'), (route) => false);
-                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
               },
             ),
           ],
@@ -137,14 +83,13 @@ class Helper {
       backgroundColor: AppColors().colorPrimary(1),
     );
   }
+
   static Widget showLoader({Color? color}) {
     return SpinKitFadingCircle(
       itemBuilder: (BuildContext context, int index) {
         return DecoratedBox(
-          decoration: BoxDecoration(
-            color: color ?? AppColors()
-                .colorPrimary(1)
-          ),
+          decoration:
+              BoxDecoration(color: color ?? AppColors().colorPrimary(1)),
         );
       },
     );
